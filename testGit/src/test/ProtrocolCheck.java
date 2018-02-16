b461be355de3d4b455492057ae684a3c570c33c3
gowthamid <ggowthii@gmail.com>
2018-02-16 21:58:29 +0530

    Update ProtrocolCheck.java

--- testGit/src/test/ProtrocolCheck.java
+++ testGit/src/test/ProtrocolCheck.java
@@ -39,7 +39,7 @@ public class ProtrocolCheck {
 				  }  
 
 			  } }; 
-			InetAddress ip=InetAddress.getByName("10.77.34.157");
+			InetAddress ip=InetAddress.getByName(""); //add ip address as a param
 			//System.out.println(ip.getHostName()+" "+ip.getHostAddress()+" "+ip.getAddress());
 			SSLContext ctx = SSLContext.getInstance("SSL");
 			ctx.init(null, trustManager, null);
